b35b6ca639f792ffbc1c2d3d23d76df3e781da6e
ahmddp <ahmddp@gmail.com>
2019-01-07 17:43:37 +0300

    Update config-jenkins.sh
    --allow-unauthenticated is added

--- config-jenkins.sh
+++ config-jenkins.sh
@@ -21,7 +21,7 @@ sudo apt-get install docker-ce -y
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
 sudo apt-get install apt-transport-https -y
-sudo apt-get update && sudo apt-get install azure-cli -y
+sudo apt-get update && sudo apt-get install azure-cli -y --allow-unauthenticated
 
 # Kubectl
 cd /tmp/
